1f97c645088fc402e4758af483b457fcd77463da
sidaliadbez <hs_zebda@esi.dz>
2020-07-13 18:43:55 +0100

    button and sounds added, V1

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,61 @@ class xylophone extends StatelessWidget {
     return  MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-                child: FlatButton(
+            child: Column(
+                children: <Widget>[
+                  FlatButton(
+                      color: Colors.red[100],
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('note1.wav');
+                  },
+                  child: Text("HELLO"),
+
+                ),
+                  FlatButton(
+                      color: Colors.red[200],
                   onPressed: () {
                     final player = AudioCache();
                     player.play('note2.wav');
                   },
-                  child: Text("HELLO"),
-                ))),
+                  child: Text("HELLO")),
+                  FlatButton(
+                      color: Colors.red[300],
+                      onPressed: () {
+                        final player = AudioCache();
+                        player.play('note3.wav');
+                      },
+                      child: Text("HELLO")),
+                  FlatButton(
+                      color: Colors.red[400],
+                      onPressed: () {
+                        final player = AudioCache();
+                        player.play('note4.wav');
+                      },
+                      child: Text("HELLO")),
+                  FlatButton(
+                      color: Colors.red[500],
+                      onPressed: () {
+                        final player = AudioCache();
+                        player.play('note5.wav');
+                      },
+                      child: Text("HELLO")),
+                  FlatButton(
+                      color: Colors.red[600],
+                      onPressed: () {
+                        final player = AudioCache();
+                        player.play('note6.wav');
+                      },
+                      child: Text("HELLO")),
+                  FlatButton(
+                      color: Colors.red[700],
+                      onPressed: () {
+                        final player = AudioCache();
+                        player.play('note7.wav');
+                      },
+                      child: Text("HELLO"))
+              ],
+            )),
       ),
     );
   }
